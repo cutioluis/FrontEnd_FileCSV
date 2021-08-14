763e3c43ddfeca027651d45a3b5a124efffeb528
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-14 15:08:07 -0500

    ADD useEffect and axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React , {useEffect} from 'react'
-import './App.css';
+import React, { useEffect  } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
-     
-  
+
+  const getData = async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    console.log(res.data);
+  }
 
   useEffect(() => {
-  },[])
+    getData();
+  }, [])
+
   return (
     <div className="App">
       <h1>Coin Market</h1>
