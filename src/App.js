2c8c78cce23b3aac8b016d778ca7609cdc9dac6d
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-14 15:34:10 -0500

    ADD table coins name

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useEffect  } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+import TableCoins from "./components/TableCoins";
 
 function App() {
+  const [coins, setCoins] = useState([]);
 
   const getData = async () => {
-    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    console.log(res.data);
-  }
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    setCoins(res.data);
+  };
 
   useEffect(() => {
     getData();
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      <h1>Coin Market</h1>
+    <div className="container">
+      <div className="row">
+        <TableCoins coins={coins} />
+      </div>
     </div>
   );
 }
---
+++ src/components/TableCoins.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TableCoins = ({ coins }) => {
+  return (
+    <table className="table table-dark mt-4 table-hover">
+      <thead>
+        <tr>
+          <td>Name :</td>
+        </tr>
+      </thead>
+      <tbody>
+        {coins.map((coin) => (
+          <tr key={coin.name}>
+              <td>{coin.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableCoins;
