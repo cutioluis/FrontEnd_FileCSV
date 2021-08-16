366b33f562e306c29e496668a3cef6d528a0e4eb
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-16 15:24:38 -0500

    ADD new sections in coinRow component

--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,18 +1,23 @@
 import React from "react";
+import CoinRow from './CoinRow'
+
+const titles = ["#", "Coin", "Price", "Price Change", "24h Volume"];
 
 const TableCoins = ({ coins }) => {
   return (
     <table className="table table-dark mt-4 table-hover">
       <thead>
         <tr>
-          <td>Name :</td>
+          {
+            titles.map((title) => (
+              <td key={title}>{title}</td>
+            ))
+          }
         </tr>
       </thead>
       <tbody>
-        {coins.map((coin) => (
-          <tr key={coin.name}>
-              <td>{coin.name}</td>
-          </tr>
+        {coins.map((coin, index) => (
+          <CoinRow coin={coin} index={index + 1 } key={index } />
         ))}
       </tbody>
     </table>
